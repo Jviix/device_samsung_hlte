91367a3dfa706012d391c376a4b7542aec8bcd8c
Jviix <33033405+Jviix@users.noreply.github.com>
2018-08-30 19:08:05 +0200

    Update aosp_hlte.mk

--- aosp_hlte.mk
+++ aosp_hlte.mk
@@ -14,16 +14,15 @@
 # limitations under the License.
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
 PRODUCT_DEVICE := hlte
-PRODUCT_NAME := aosp_hlte
+PRODUCT_NAME := pixys_hlte
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_ARCH := arm
 TARGET_DENSITY := xxhdpi
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm
 
