fc8eae66c63305dac796441690d02e5b0a54aab6
Jviix <33033405+Jviix@users.noreply.github.com>
2018-10-06 16:31:31 +0200

    Update init_hlte.cpp

--- init/init_hlte.cpp
+++ init/init_hlte.cpp
@@ -39,6 +39,7 @@
 using android::base::GetProperty;
 using android::init::property_set;
 
+
 #define ISMATCH(a, b) (!strncmp((a), (b), PROP_VALUE_MAX))
 
 void init_target_properties()
@@ -55,19 +56,15 @@ void init_target_properties()
         property_override("ro.build.description", "hltexx-user 5.0 LRX21V N9005XXSGBQD5 release-keys");
         property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-N9005");
         property_override_dual("ro.product.device", "ro.vendor.product.device", "hlte");
-        gsm_properties("gsm");
     } else if (bootloader.find("N900P") == 0) {
         /* hltespr - Sprint */
         property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/hltespr/hltespr:5.0/LRX21V/N900PVPSEPL1:user/release-keys");
         property_override("ro.build.description", "hltespr-user 5.0 LRX21V N900PVPSEPL1 release-keys");
         property_override_dual("ro.product.model", "ro.vendor.product.model", "SM-N900P");
         property_override_dual("ro.product.device", "ro.vendor.product.device", "hltespr");
-        cdma_properties("Sprint", "310120", "8", "1", "spr");
     } else {
-        gsm_properties("gsm");
-    }
-
     std::string device = GetProperty("ro.product.device", "");
     LOG(ERROR) << "Found bootloader id " << bootloader <<  " setting build properties for "
         << device <<  " device" << std::endl;
+}
 }
